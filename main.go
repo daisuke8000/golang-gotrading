dab4da164a40566f47422ee64684fc5ae002a90c
daisuke8000 <daisuke12108000@gmail.com>
2020-08-13 12:10:33 +0900

    api認証及びgetbalanceAPIの作成

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"gotrading_application/112_sma/bitflyer"
 	"gotrading_application/112_sma/config"
 	"gotrading_application/112_sma/utils"
-	"log"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
-	//fmt.Println(config.Config.ApiKey)
-	//fmt.Println(config.Config.ApiSecret)
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	fmt.Println(apiClient.GetBalance())
 }
